7ac32fc8c70eed0ec92307dec801adf16b1b8c41
Hasibul Hossain <hasibulhossain52@gmail.com>
2022-04-29 16:49:54 +0600

    update:

--- lib/main.dart
+++ lib/main.dart
@@ -34,13 +34,24 @@ class _HomeScreenState extends State<HomeScreen>
 
   AnimationController? _controller;
 
+  Animation<double>? _animation;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    _controller =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.easeInCubic,
+      ),
+    );
   }
 
   void toggle() {
@@ -49,28 +60,48 @@ class _HomeScreenState extends State<HomeScreen>
 
   @override
   Widget build(BuildContext context) {
+    final sh = MediaQuery.of(context).size.height;
+    final sw = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: GestureDetector(
-        onTap: toggle,
+      body: Container(
+        color: Colors.red,
         child: AnimatedBuilder(
-          animation: _controller!,
+          animation: _animation!,
           builder: (context, child) {
-            double slide = _controller!.value * translateTo;
-            double scale = 1 - (_controller!.value * 0.5);
+            double slide = _animation!.value * sw / 2;
+            double scale = 1 - (_animation!.value * 0.5);
             return Stack(
               children: [
                 Container(
+                  height: sh,
+                  width: sw,
                   color: Colors.blue,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 80),
+                    child: Text('App Drawer', style: Theme.of(context).textTheme.headline3,),
+                  ),
                 ),
                 Transform(
+                  alignment: Alignment.centerLeft,
                   transform: Matrix4.identity()
                     ..translate(slide)
                     ..scale(scale),
-                  alignment: Alignment.centerLeft,
                   child: Container(
-                    color: Colors.amber,
+                    height: sh,
+                    width: sw,
+                    color: Colors.pink,
+                    child: GestureDetector(
+                      onTap: toggle,
+                      child: Center(
+                        child: Text(
+                          'Tap Here\nApp Screen',
+                          style: Theme.of(context).textTheme.headline3,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                )
               ],
             );
           },
